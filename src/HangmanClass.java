8dd2cc550a5a8b64785394a8c52c06e0d646011a
Yoana Lalkovska <yoyoyo3773@gmail.com>
2019-01-31 21:44:05 +0200

    working with files...ugh

--- src/HangmanClass.java
+++ src/HangmanClass.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class HangmanClass {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Hangman! The computer will think of a city/village " +
+                "in Bulgaria in Bulgarian and you gotta guess it.");
 
         boolean doYowantToPlay = true;
         while (doYowantToPlay) {
@@ -12,7 +14,7 @@ public class HangmanClass {
 
             }
             System.out.println();
-            System.out.println("Искате ли още една игра? Натиснете Y");
+            System.out.println("Wanna play again? Press Y");
             Character response = (sc.next().toUpperCase()).charAt(0);
             doYowantToPlay = (response == 'Y');
 
--- src/HangmanContinue.java
+++ src/HangmanContinue.java
@@ -1,2 +1,48 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class HangmanContinue {
+    String mysteryWord;
+    StringBuilder currentGuess;
+    ArrayList<Character> previousGuesses = new ArrayList<>();
+    int maxTries = 6;
+    int currentTry = 0;
+
+    ArrayList<String> dictionary = new ArrayList<>();
+    private static FileReader fileReader;
+    private static BufferedReader bufferedFileReader;
+
+    public HangmanContinue () throws IOException{
+        initializesStreams ();
+        mysteryWord = pickWord ();
+        currentGuess = initializeCurrentGuess();
+
+    }
+    public void initializesStreams() throws IOException {
+        try {
+            File inFile = new File("dictionary.txt");
+            fileReader = new FileReader(inFile);
+            bufferedFileReader = new BufferedReader(fileReader);
+            String currentLine = bufferedFileReader.readLine();
+            while (currentLine!=null){
+                dictionary.add (currentLine);
+
+            }
+            bufferedFileReader.close();
+            fileReader.close();
+        }
+        catch (IOException e){
+            System.out.println("Couldn't init streams");
+        }
+    }
+    public String pickWord(){
+        Random rand = new Random();
+        int wordIndex = Math.abs (rand.nextInt())%dictionary.size();
+        return dictionary.get(wordIndex);
+    }
 }
